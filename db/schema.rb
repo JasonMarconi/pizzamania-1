b08c17db080c32161667372534ccd51e76df4f6d
Jason Marconi <jason.marconi0123@gmail.com>
2017-12-10 12:26:15 +0530

    Model code generated

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171209061714) do
+ActiveRecord::Schema.define(version: 20171210065529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bakers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cashiers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "managers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/factories/bakers.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :baker do
+    
+  end
+end
